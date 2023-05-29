f788921a4ac9ef71d6ae84e1ad585bee9f7d2d7f
Sergey Barkhalev <dr.barkhalev@yandex.ru>
2023-05-29 20:59:13 +0500

    Добавил MagicBox Generics

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -1,5 +1,18 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MagicBox<String> stringMagicBox =  new MagicBox<>(4);
+
+        stringMagicBox.add("123");
+        stringMagicBox.add("qwerty");
+
+//        stringMagicBox.pick();
+
+        stringMagicBox.add("someString");
+        stringMagicBox.add("magicString");
+
+        String pick = stringMagicBox.pick();
+        System.out.println(pick);
     }
+
 }
